e5536293e0f1508aa3789cb48c526ae553e054e2
Matías Soto Lomboy <mati.sotitol@gmail.com>
2024-09-25 15:07:24 -0300

    added hola mundo
    hola mundo donen libros porfa

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: "Home",
+    description: "Esta es la página de inicio",
+};
+  
+export default function Home() {
+    return (
+      <>
+        <div className="container">
+          <h1 className="title">Hola mundo!</h1>
+        </div>
+      </>
+    );
+  }
+  
