9449c255d1dc9e5c7ca48cba84ae3c279a282a87
cris-tobal <jcristobal2002@gmail.com>
2024-11-07 18:06:22 -0300

    cambiado el <a></a> de BookCard por => router.push() segun documentacion de next

--- app/book_cards/book-cards.js
+++ app/book_cards/book-cards.js
@@ -1,15 +1,18 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
+
 export default function BookCard({ libro }) {
     const portadaLib = (libro.item.caratula != "") ? libro.item.caratula : "https://placehold.co/150x240";
+    const router = useRouter();
 
     return(
-        <a href={`/pdrl/${libro.item.id}`}>
-        <div className="book-card">
+        <div className="book-card" onClick={() => router.push(`/pdrl/${libro.item.id}`)}>
             <img src={portadaLib} className="portada" alt={libro.item.titulo}></img>
             <div className="pie">
             <p className="titulo">{libro.item.titulo}</p>
             <p className="autor">{libro.item.autores}</p>
             </div>
         </div>
-        </a>
     )
 };
