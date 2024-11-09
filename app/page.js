91762fb5ddf6718b4550695c9f3d4085fc8eda20
Matías Soto Lomboy <mati.sotitol@gmail.com>
2024-11-09 18:48:39 -0300

    test

--- app/page.js
+++ app/page.js
@@ -1,26 +1,32 @@
 import BookCard from "./book_cards/book-cards.js"
 import libros from "./libros.json"
+import Image from "next/image.js";
 
 export const metadata = {
-    title: "Home",
-    description: "Esta es la página de inicio",
+    title: "DLP - catálogo",
+    description: "Catálago de la biblioteca independiente DLP library",
 };
   
 export default function Home() {
-  libros.map((item) => {
-    console.log(item.titulo);
-  })
+    libros.map((item) => console.log(item.titulo))
+
     return (
-      <>
+    <>
         <div className="container">
-          <h1 className="title">catálogo**</h1>
-          <div className="book-shelf">
+        {/*<h1 className="title">catálogo**</h1>*/}
+        <Image
+            src = "/public/assets/logoBlanco.png"
+            alt = "Logo épico"
+            width = {500}
+            height = {250}
+        />
+        <div className="book-shelf">
             {libros.map((item, cont) => 
-              (<BookCard libro={{item}} key={cont}></BookCard>
+            (<BookCard libro={{item}} key={cont}></BookCard>
             ))}
-          </div>
         </div>
-      </>
+        </div>
+    </>
     );
-  }
+}
   
