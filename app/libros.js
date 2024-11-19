a2ba091935d342ea014a64b9f88f10d36aeb7fe1
mToledo <136205716+Tolally@users.noreply.github.com>
2024-11-19 13:54:16 -0300

    otro try

---
+++ app/libros.js
@@ -0,0 +1,30 @@
+// https://github.com/vercel/next.js/issues/48344#issuecomment-1653612812
+'use client'
+import fetchData from ".//api";
+import React from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import { LOOP } from '@splidejs/splide';
+
+  
+const ListOfBooks = async () => {
+    const data = await fetchData(); // Esperamos a que la función fetchData termine de ejecutarse
+
+    return (
+      <div className="wrapper">
+      <h2 id="basic-example-heading">Recomendaciones</h2>
+        {
+          data?.map(books =>(
+              <div className="book-card">
+                <div className="pie">
+                  <img src={`data:image/jpeg;base64,${(books.caratula)}`} width="100px" height="180px"/>
+                  <p className="titulo">{books.titulo}</p>
+                  <p className="autor">{books.autores}</p>
+                </div>
+              </div>
+          ))};
+    </div>
+    );
+  };
+
+  export default ListOfBooks;
