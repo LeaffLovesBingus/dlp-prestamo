0269f60b1937d82f21a3b427d70d58ebf4f6b050
mToledo <136205716+Tolally@users.noreply.github.com>
2024-11-19 12:54:00 -0300

    test

--- app/page.js
+++ app/page.js
@@ -7,12 +7,12 @@ export const metadata = {
   
 export default function Home() {
     const data = await fetchData(); // Esperamos a que la función fetchData termine de ejecutarse
-    
+
     return (
       <>
         <div className="container">
           <h1 className="title">Hola mundo!</h1>
-          <h1 className="title">Que pasa realmente?</h1>
+          <h1 className="title">Que pasa realmente?!</h1>
           <h1 className="title">Adios mundo!</h1>
         </div>
       </>
