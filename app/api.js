4651dcb0a018b3a8c8dfc76ab7caff153b3c920c
mToledo <136205716+Tolally@users.noreply.github.com>
2024-11-19 12:51:59 -0300

    itento

---
+++ app/api.js
@@ -0,0 +1,23 @@
+
+
+const fetchData = async () => {
+    try {
+      const data = await fetch("https://dlp-api.vercel.app/libros", { method: "GET" });
+      //const res = await import(".//api-test/ejemplo-1", { method: "GET" });
+      //const data = res.libros;
+      const data_parsed = await data.json();
+      
+      if (response.ok) {
+        console.log('Todo bien');
+      } else {
+        console.log('Respuesta de red OK pero respuesta de HTTP no OK');
+      }
+      return data_parsed;
+  
+    } catch (error) {
+      console.log('Hubo un problema con la petición Fetch:' + error.message);
+    }
+    
+};
+
+export default fetchData;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import fetchData from ".//api";
+
 export const metadata = {
     title: "Home",
     description: "Esta es la página de inicio",
 };
   
 export default function Home() {
+    const data = await fetchData(); // Esperamos a que la función fetchData termine de ejecutarse
+    
     return (
       <>
         <div className="container">
