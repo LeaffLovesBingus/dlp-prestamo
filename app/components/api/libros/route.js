4b26306c1b0ead564ef92690f49525c3454e2a40
Matías Soto Lomboy <mati.sotitol@gmail.com>
2024-11-27 01:44:45 -0300

    Revert "funciona?"
    This reverts commit 1d90a9372d19232bb7d486f8bd08db08ec174d32.

--- app/components/api/libros/route.js
+++ app/components/api/libros/route.js
@@ -1,6 +1,8 @@
-export async function GET(params){
-    const id = params.url.split("?")[1].split("=")[1];
-    const url = `https://dlp-api.vercel.app/libros?id=${id}`;
+export async function GET(params) {
+    let url = `https://dlp-api.vercel.app/libros`;
+    const id = params.url.split("?id=")[1];
+    console.log(id);
+    if (!isNaN(parseInt(id))) url = `https://dlp-api.vercel.app/libros?id=${id}`;
     const res = await fetch(url);
     const data = await res.json();
     return Response.json(data);
