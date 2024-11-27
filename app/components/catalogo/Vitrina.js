5665555d80d4b0927b7ac77f00cef3a2ac656bed
mToledo <136205716+Tolally@users.noreply.github.com>
2024-11-27 08:19:56 -0300

    asd

--- app/components/catalogo/Vitrina.js
+++ app/components/catalogo/Vitrina.js
@@ -1,5 +1,5 @@
 "use client";
-import { fetchCatalogo } from "../endpoint";
+import { fetchCatalogo} from '../components';
 import '@splidejs/react-splide/css';
 import TarjetaLibro from './TarjetaLibro';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
