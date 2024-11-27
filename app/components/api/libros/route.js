a594fbbf6082a50653ac4667d072a0e76aefab6c
mToledo <136205716+Tolally@users.noreply.github.com>
2024-11-27 08:05:57 -0300

    halp

--- app/components/api/libros/route.js
+++ app/components/api/libros/route.js
@@ -1,6 +1,9 @@
 export async function GET(params){
     const id = params.url.split("?")[1].split("=")[1];
-    const url = `https://dlp-api.vercel.app/libros?id=${id}`;
+    const url = "https://dlp-api.vercel.app/libros";
+    if (id != undefined){
+      url = `https://dlp-api.vercel.app/libros?id=${id}`;
+    };
     const res = await fetch(url);
     const data = await res.json();
     return Response.json(data);
