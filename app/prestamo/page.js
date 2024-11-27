60bd92940697be0a1fac950bf84abf68d23d907f
shyupss_ <diegosotovergara17@gmail.com>
2024-11-27 04:33:49 -0300

    ajuste-css-prestamo

--- app/prestamo/page.js
+++ app/prestamo/page.js
@@ -33,7 +33,7 @@ export default function Home() {
 
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=keyboard_double_arrow_left" />
 
-      <header className='header'>
+      <header className='headerPrestamo'>
 
         <div className="titleContainer">
           <div className="imageWrapper-01">
