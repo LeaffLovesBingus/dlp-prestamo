2b4f4b68a1948ae350425f83bcc64209d3ac0315
cris-tobal <jcristobal2002@gmail.com>
2024-11-27 07:45:06 -0300

    NextResponse

--- app/components/api/libros/route.js
+++ app/components/api/libros/route.js
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function GET(params) {
     let url = `https://dlp-api.vercel.app/libros`;
     const reqUrl = params.url;
@@ -5,8 +7,7 @@ export async function GET(params) {
         const id = reqUrl.split("?id=")[1];
         if (!isNaN(parseInt(id))) url = `https://dlp-api.vercel.app/libros?id=${id}`;
     }
-    console.log([reqUrl, url]);
     const res = await fetch(url);
     const data = await res.json();
-    return Response.json(data);
+    return NextResponse.json(data);
 }
